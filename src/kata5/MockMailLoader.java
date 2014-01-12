942ad7549ff5a56174470e6444c8149ce12f85fa
Quique <Quique@10.230.169.43>
2014-01-12 12:40:25 +0000

    Subiendo katas a github

---
+++ src/kata5/MockMailLoader.java
@@ -0,0 +1,15 @@
+package kata5;
+
+public class MockMailLoader implements MailLoader {
+
+    @Override
+    public Mail[] load() {
+        Mail[] mails = new Mail[5];
+        mails[0] = new Mail("prueba@gmail.com");
+        mails[1] = new Mail("prueba@hotmail.com");
+        mails[2] = new Mail("prueba@hotmail.com");
+        mails[3] = new Mail("prueba@gmail.com");
+        mails[4] = new Mail("prueba@outlook.com");
+        return mails;
+    }
+}
